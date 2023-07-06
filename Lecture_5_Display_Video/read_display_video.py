d0089c5864220b37947e2154ef0e455eced46d88
Rahul Jain <rahuljain809@gmail.com>
2023-07-06 09:51:18 -0700

    YOLO NAS Tutorial

--- Lecture_5_Display_Video/read_display_video.py
+++ Lecture_5_Display_Video/read_display_video.py
@@ -1,2 +1,12 @@
 import cv2
 
+cap = cv2.VideoCapture("../videos/bikes.mp4")
+
+while cap.isOpened():
+    ret_, frame = cap.read()
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) & 0xFF==ord('1'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
