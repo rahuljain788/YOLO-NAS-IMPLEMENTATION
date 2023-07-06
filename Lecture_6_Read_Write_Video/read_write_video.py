aaf594ddd3d128e7ad6e99a7f13fb94e47525dcf
Rahul Jain <rahuljain809@gmail.com>
2023-07-06 10:15:00 -0700

    YOLO NAS Tutorial

--- Lecture_6_Read_Write_Video/read_write_video.py
+++ Lecture_6_Read_Write_Video/read_write_video.py
@@ -2,16 +2,24 @@ import cv2
 
 cap = cv2.VideoCapture("../videos/bikes.mp4")
 
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+fourcc = cv2.VideoWriter_fourcc(*"DIVX")
+
+out = cv2.VideoWriter('../OutputVideo/Output.avi', fourcc, 10, (frame_width, frame_height))
+
 while cap.isOpened():
     ret_, frame = cap.read()
     if ret_:
         # resize the image to 50% of original size to fit on the screen
         frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
         cv2.imshow("Video", frame)
+        out.write(frame)
         if cv2.waitKey(1) & 0xFF==ord('1'):
             break
     else:
         break
 
+out.release()
 cap.release()
 cv2.destroyAllWindows()
