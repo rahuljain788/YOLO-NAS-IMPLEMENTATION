9a4179ef39927c94d36d825ed07e9ba8aeb83fc9
Rahul Jain <rahuljain809@gmail.com>
2023-07-06 12:26:58 -0700

    YOLO NAS Tutorial

--- Lecture_8_Object_Tracking/object_tracking.py
+++ Lecture_8_Object_Tracking/object_tracking.py
@@ -72,14 +72,6 @@ while cap.isOpened():
                 identities = tracker_dets[:,8]
                 categories = tracker_dets[:,4]
                 draw_boxes(frame, bbox_xyxy, identities, categories)
-                # label = f"{classNames[int(cls)]}{conf}"
-                # t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                # c2 = x1 + t_size[0], y1 - t_size[1] - 3
-                # print("Frame N ", count, "", x1, y1, x2, y2, label, c2, t_size)
-                #
-                # cv2.rectangle(frame, (x1,y1), c2, (255, 0, 255), -1, cv2.LINE_AA)
-                # cv2.rectangle(frame, (x1,y1), (x2,y2), (255, 0, 255), 3)
-                # cv2.putText(frame, label, (x1, y1-2), 0, 1, [255,255,255], thickness=1, lineType=cv2.LINE_AA)
             cv2.imshow("Video", frame)
             if cv2.waitKey(1) & 0xFF==ord('1'):
                 break
